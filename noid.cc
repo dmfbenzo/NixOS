c3d85f3293b2468f013cc2ff14d61a41853d0ed8
dmfbenzo <dmfbenzo@gmail.com>
2022-07-06 10:34:00 -0500

    Create noid.cc

---
+++ noid.cc
@@ -0,0 +1,23 @@
+struct noid_lst
+{
+  noid *t_noid;
+  noid *n_noid;
+};
+
+
+noid::noid()
+{
+  
+};
+noid::~noid()
+{
+};
+
+int noid::saySomething()
+{
+
+  printf("hi.\r\n");
+  
+  return 0;
+
+};
